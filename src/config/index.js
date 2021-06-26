66982fd2a292b5af1faa6f189d6f76fcdf4d6703
vitorlc <vitor_cirqueira1@hotmail.com>
2021-06-26 12:16:34 -0300

    Feat: configure express server

---
+++ src/config/index.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+
+const envFound = dotenv.config();
+if (envFound.error) {
+  throw new Error("Couldn't find .env file");
+}
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 3000;
+
+module.exports = {
+
+  port: parseInt(PORT),
+  api: {
+    prefix: '/api',
+  },
+  db: {
+    development: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT || 'mysql',
+      port: process.env.DB_PORT,
+    }
+  },
+  env: {
+    NODE_ENV: process.env.NODE_ENV,
+  }
+};
---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const config = require('./config');
+const routes = require('./routes')
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('short'));
+app.use(express.json());
+
+app.listen(config.port, (err) => {
+  if (err) {
+    console.log(err)
+    process.exit(1)
+    return
+  }
+  console.log(`Server listening on port: ${config.port}`)
+});
