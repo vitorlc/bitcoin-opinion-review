a3cc873b79b41f690bb21fc281d5b23a8937b717
vitorlc <vitor_cirqueira1@hotmail.com>
2021-06-27 11:40:17 -0300

    Feat: add services

--- src/controllers/opinionController.js
+++ src/controllers/opinionController.js
@@ -1,8 +1,11 @@
+const opinionService = require('../services/opinionService')
 
 module.exports = {
   async saveOpinion(req, res) {
     try {
-      res.status(200).json(product)
+      const { body } = req
+      const created = await opinionService.saveOpinion(body)
+      res.status(200).json(created)
     } catch (e) {
       console.log(e)
       res.status(500).json({ error: true, message: "Não foi possível salvar a opinião" })
@@ -10,7 +13,9 @@ module.exports = {
   },
   async getOpinion(req, res) {
     try {
-      res.status(200).json(data)
+      const { author } = req.params
+      const opinion = await opinionService.getOpinion(author)
+      res.status(200).json(opinion)
     } catch (e) {
       console.log(e)
       res.status(500).json({ error: true, message: "Não foi possível encontrar a opinião" })
---
+++ src/services/opinionService.js
@@ -0,0 +1,50 @@
+const db = require('../models');
+const Opinions = db.opinions;
+const vader = require('vader-sentiment');
+
+const analyzeTextPolarity = (text) => {
+  const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(text)
+
+  let opinion
+
+  if (intensity.compound >= 0.05) opinion = 'positive'
+  else if (intensity.compound > -0.05 && intensity.compound < 0.05) opinion = 'neutral'
+  else if (intensity.compound <= -0.05) opinion = 'negative'
+
+  return {
+    opinion,
+    avg: intensity.compound
+  }
+}
+
+const saveOpinion = (data) => {
+  const { opinion, avg } = analyzeTextPolarity(data.text)
+  return Opinions.create({
+    author: data.author,
+    text: data.text,
+    link: data.link,
+    date: data.date,
+    opinion,
+    avg
+  })
+}
+
+const getOpinion = (author) => Opinions.findOne({
+  where: {
+    author
+  }
+})
+
+const deleteOpinion = (id) => Opinions.destroy({
+  where: {
+    id
+  }
+})
+
+
+module.exports = {
+  saveOpinion,
+  getOpinion,
+  deleteOpinion,
+  analyzeTextPolarity
+}
