8baf94e543660e1226377312b030d5d507924ccd
vitorlc <vitor_cirqueira1@hotmail.com>
2021-06-27 11:40:41 -0300

    Feat: add tests

--- src/config/index.js
+++ src/config/index.js
@@ -21,6 +21,14 @@ module.exports = {
       host: process.env.DB_HOST,
       dialect: process.env.DB_DIALECT || 'mysql',
       port: process.env.DB_PORT,
+    },
+    test: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT || 'mysql',
+      port: process.env.DB_PORT,
     }
   },
   env: {
---
+++ src/test/opinions.test.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+const crypto = require('crypto');
+const config = require('../config');
+
+const opinionService = require('../services/opinionService');
+
+const generate = () => crypto.randomBytes(20).toString('hex');
+
+const request = (url, method, data) => axios({ url, method, data });
+
+test('Should get opinions', async () => {
+  const opinion1 = await opinionService.saveOpinion({
+    author: generate(),
+    text: "Beginning is easy, continuing is hard. -Japanese proverb on #Bitcoin",
+    link: generate(),
+    data: "2021-06-27"
+  });
+
+  const response = await request(`http://localhost:${config.port}/api/opinions/${opinion1.author}`, 'get');
+  const opinions = response.data;
+
+  expect(opinions).toHaveProperty('opinion');
+  expect(opinions).toHaveProperty('avg');
+
+  await opinionService.deleteOpinion(opinion1.id);
+})
+
+test('Should save opinions', async () => {
+  const data = {
+    author: generate(),
+    text: "Beginning is easy, continuing is hard. -Japanese proverb on #Bitcoin",
+    link: generate(),
+    date: "2021-06-27"
+  };
+
+  const response = await request(`http://localhost:${config.port}/api/opinions`, 'post', data);
+  const opinion = response.data;
+
+  expect(opinion.author).toBe(data.author);
+  expect(opinion.text).toBe(data.text);
+  expect(opinion.link).toBe(data.link);
+  expect(opinion.date).toBe(data.date);
+
+  await opinionService.deleteOpinion(opinion.id);
+})
